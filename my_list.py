50bcf35aeb049e3d8e9b6ff6df7ea8f59dc77af2
basharat <basharatyousufi786@gmail.com>
2023-07-24 08:05:06 +0500

    my assignment

---
+++ my_list.py
@@ -0,0 +1,18 @@
+# Assinmgnment #10:
+#exerciz 1 : list operatinons 
+#1- create an emty list called my_list .
+#2-Append the folloing elemnts to the list:10, 20, 30, 40, 50,.
+#3-insert the number 15 at index 1 and 35 at above opertions.
+#4- print the list after performing the above operations.
+
+
+my_list =[]
+my_list.append("10,20,30,40,,50,")
+print(my_list)
+
+my_list=[10,20,30,40,50,]
+my_list=[1,15]
+my_list.insert(4,35)
+print(my_list)
+
+
